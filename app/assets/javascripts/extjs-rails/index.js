8826585a6e893aeeb38011b7244a8b6aa0ebec93
Ryan Lovelett <ryan@wahvee.com>
2012-06-02 09:20:20 -0500

    Installed Ext JS 4.1.0 GPL.

--- app/assets/javascripts/extjs-rails/index.js
+++ app/assets/javascripts/extjs-rails/index.js
@@ -1,1 +1,15 @@
+// JSB3 'foundation' files
+//= require ./src/core/src/Ext.js
+//= require ./src/core/src/version/Version.js
+//= require ./src/core/src/lang/String.js
+//= require ./src/core/src/lang/Number.js
+//= require ./src/core/src/lang/Array.js
+//= require ./src/core/src/lang/Function.js
+//= require ./src/core/src/lang/Object.js
+//= require ./src/core/src/lang/Date.js
+//= require ./src/core/src/class/Base.js
+//= require ./src/core/src/class/Class.js
+//= require ./src/core/src/class/ClassManager.js
+//= require ./src/core/src/class/Loader.js
+//= require ./src/core/src/lang/Error.js
 //= require_tree .
