7bf8cb809feb55d0a1cd55f7a27593177431d806
Ryan Lovelett <ryan@wahvee.com>
2012-06-03 13:19:41 -0500

    Include the sdk.jsb3 file dynamically.

--- lib/extjs-rails.rb
+++ lib/extjs-rails.rb
@@ -3,5 +3,21 @@ require "extjs-rails/version"
 module Extjs
   module Rails
     require 'extjs-rails/engine' if defined?(Rails)
+
+    def self.root
+      File.expand_path '../..', __FILE__
+    end
+
+    def self.lib
+      File.join root, 'lib'
+    end
+
+    def self.app
+      File.join root, 'app'
+    end
+
+    def self.javascripts
+      File.join app, 'assets/javascripts/extjs-rails'
+    end
   end
 end
